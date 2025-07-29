2e905567e64991428794b3bfc3145de252305e45
yon1986 <jhonnyfarfanoficial@gmail.com>
2025-07-29 12:53:37 -0600

    Integración de UserProvider de World App

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Importamos el UserProvider del SDK de World App
+import { UserProvider } from '@worldcoin/id'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </StrictMode>
 )
